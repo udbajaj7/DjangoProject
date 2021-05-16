cb0bdb1d8ff10ce52af7cbc2135c3cf37c6d6c5f
udbajaj7 <67692705+udbajaj7@users.noreply.github.com>
2021-05-16 19:28:05 +0530

    Delete employee directory

--- employee/forms.py
+++
@@ -1,9 +0,0 @@
-# forms.py
-from django import forms
-from .models import Employee
-
-class EmployeeForm(forms.ModelForm):
-
-    class Meta:
-        model = Employee
-        fields = ['emp_image']
